9c7163ba861f26ecf16369f72134b47f8bd3d5e7
Sakshi <sakshi.agg428@gmail.com>
2024-07-15 21:52:53 +0530

    separated Payload into Payload class

---
+++ src/main/java/Payload/Payloads.java
@@ -0,0 +1,24 @@
+package Payload;
+
+public class Payloads {
+
+    public static String addPlace(){
+
+        return "{\n" +
+                "  \"location\": {\n" +
+                "    \"lat\": -38.383494,\n" +
+                "    \"lng\": 33.427362\n" +
+                "  },\n" +
+                "  \"accuracy\": 50,\n" +
+                "  \"name\": \"Frontline house\",\n" +
+                "  \"phone_number\": \"(+91) 983 893 3937\",\n" +
+                "  \"address\": \"29, side layout, cohen 09\",\n" +
+                "  \"types\": [\n" +
+                "    \"shoe park\",\n" +
+                "    \"shop\"\n" +
+                "  ],\n" +
+                "  \"website\": \"http://google.com\",\n" +
+                "  \"language\": \"French-IN\"\n" +
+                "}";
+    }
+}
--- src/test/java/Basic/Basics.java
+++ src/test/java/Basic/Basics.java
@@ -1,7 +1,9 @@
 package Basic;
 
+import Payload.Payloads;
 import io.restassured.RestAssured;
 import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.equalTo;
 
 public class Basics {
     public static void main(String[] args) {
@@ -12,23 +14,9 @@ public class Basics {
         //Then - validate the response
         RestAssured.baseURI = "https://rahulshettyacademy.com/";
         given().log().all().queryParam("key","qaclick123").header("Content-Type","application/json")
-                .body("{\n" +
-                        "  \"location\": {\n" +
-                        "    \"lat\": -38.383494,\n" +
-                        "    \"lng\": 33.427362\n" +
-                        "  },\n" +
-                        "  \"accuracy\": 50,\n" +
-                        "  \"name\": \"Frontline house\",\n" +
-                        "  \"phone_number\": \"(+91) 983 893 3937\",\n" +
-                        "  \"address\": \"29, side layout, cohen 09\",\n" +
-                        "  \"types\": [\n" +
-                        "    \"shoe park\",\n" +
-                        "    \"shop\"\n" +
-                        "  ],\n" +
-                        "  \"website\": \"http://google.com\",\n" +
-                        "  \"language\": \"French-IN\"\n" +
-                        "}").when().post("maps/api/place/add/json").then().log().all()
-                .assertThat().statusCode(200);
+                .body(Payloads.addPlace()).when().post("maps/api/place/add/json").then().log().all()
+                .assertThat().statusCode(200).body("scope",equalTo("APP"))
+                .header("server", "Apache/2.4.52 (Ubuntu)");
 
     }
 }
