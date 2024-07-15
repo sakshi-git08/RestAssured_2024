5065101d1915bd3fc0a13da75d066edae574fb55
Sakshi <sakshi.agg428@gmail.com>
2024-07-15 22:03:11 +0530

    using jsonPath

--- src/test/java/Basic/Basics.java
+++ src/test/java/Basic/Basics.java
@@ -2,6 +2,8 @@ package Basic;
 
 import Payload.Payloads;
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.equalTo;
 
@@ -13,10 +15,17 @@ public class Basics {
         //when - Submit the api - resource, HTTP method
         //Then - validate the response
         RestAssured.baseURI = "https://rahulshettyacademy.com/";
-        given().log().all().queryParam("key","qaclick123").header("Content-Type","application/json")
-                .body(Payloads.addPlace()).when().post("maps/api/place/add/json").then().log().all()
+        String response = given().log().all().queryParam("key","qaclick123").header("Content-Type","application/json")
+                .body(Payloads.addPlace()).when().post("maps/api/place/add/json").then()
                 .assertThat().statusCode(200).body("scope",equalTo("APP"))
-                .header("server", "Apache/2.4.52 (Ubuntu)");
+                .header("server", "Apache/2.4.52 (Ubuntu)").extract().response().asString();
 
+        System.out.println(response);
+        JsonPath json = new JsonPath(response);
+        String placeId = json.get("place_id");
+        System.out.println(placeId);
     }
+    //Add Place -> Update Place with New Address -> Get Place to validate if New Address is
+    // present in response or not
+
 }
