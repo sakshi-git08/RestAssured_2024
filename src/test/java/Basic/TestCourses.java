04f8d81956a5ca2711a525866a432b7d1f491170
Sakshi <sakshi.agg428@gmail.com>
2024-10-12 19:42:14 +0530

    courses updates

--- src/test/java/Basic/TestCourses.java
+++ src/test/java/Basic/TestCourses.java
@@ -2,6 +2,7 @@ package Basic;
 
 import ExtraStuff.Payloads;
 import io.restassured.path.json.JsonPath;
+import org.testng.Assert;
 
 public class TestCourses {
     public static void main(String[] args) {
@@ -21,9 +22,32 @@ public class TestCourses {
         String firstCourseTitle = js.get("courses[0].title");
         System.out.println(firstCourseTitle);
         //4. Print All course titles and their respective Prices
+        for (int i = 0; i < coursesSize; i++) {
+            String courseTitle = js.getString("courses[" + i + "].title");
+            int price = js.get("courses[" + i + "].price");
+            System.out.println(courseTitle + " and it's price is : " + price);
+        }
 
         //5. Print no of copies sold by RPA Course
+//        String copies = null;
+        for (int i = 0; i < coursesSize; i++) {
+            String courseTitle = js.getString("courses[" + i + "].title");
+
+            if (courseTitle.equalsIgnoreCase("RPA")) {
+                int copies = js.getInt("courses[" + i + "].copies");
+                System.out.println(copies + " sold by RPA Course");
+                break;
+            }
+        }
         //6. Verify if Sum of all Course prices matches with Purchase Amount
+        int sum = 0;
+        for (int i = 0; i < coursesSize; i++) {
+            int price = js.get("courses[" + i + "].price");
+            int copies = js.getInt("courses[" + i + "].copies");
+            sum += price * copies;
+            System.out.println(sum);
+        }
+        Assert.assertEquals(sum, purchaseAmt);
 
     }
 }
