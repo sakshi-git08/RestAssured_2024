36ccfc70ef86272aed713493fc44bf1bcd352acd
Sakshi <sakshi.agg428@gmail.com>
2024-10-17 06:19:30 +0530

    Lib API

--- src/test/java/ExtraStuff/LibraryExcelDriven.java
+++ src/test/java/ExtraStuff/LibraryExcelDriven.java
@@ -5,15 +5,24 @@ import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.testng.annotations.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 
 public class LibraryExcelDriven {
     @Test
     public void addBook() {
+        Map<String, Object> jsonAsMap = new HashMap<>();
+        jsonAsMap.put("name", "Selenium Intro");
+        jsonAsMap.put("isbn", "Selenium");
+        jsonAsMap.put("aisle", "02");
+        jsonAsMap.put("author", "Sakshi");
         RestAssured.baseURI = "https://rahulshettyacademy.com";
         String response = given().header("Content-Type", "application/json")
-                .body("{\r\n    \"name\": \"Learn Appium Automation with Java\",\r\n    \"isbn\": \"book\",\r\n    \"aisle\": \"01\",\r\n    \"author\": \"John foer\"\r\n}\n")
+                .body(jsonAsMap)
+                //.body("{\r\n    \"name\": \"Learn Appium Automation with Java\",\r\n    \"isbn\": \"book\",\r\n    \"aisle\": \"01\",\r\n    \"author\": \"John foer\"\r\n}\n")
                 .when().post("/Library/Addbook.php").then().assertThat().statusCode(200).extract().response().asString();
         System.out.println(response);
         JsonPath json = ReUsableMethods.rawToJson(response);
